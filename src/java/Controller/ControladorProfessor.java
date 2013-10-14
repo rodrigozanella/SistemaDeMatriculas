b7c436ab825f2b4b3d18a80beea897697ca3a6cf
Rodrigo Zanella Ribeiro <rodrigo.zanella@hotmail.com.br>
2013-10-14 11:12:55 -0300

    Controlador do Professor
    Implementada a estrutura do controlador do professor (de modo geral,
    fora de um estado)

---
+++ src/java/Controller/ControladorProfessor.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Rodrigo Zanella Ribeiro
+ * @since 13/10/2013
+ * 
+ * Classe controladora das ações de um usuário professor
+ */
+public abstract class ControladorProfessor extends ControladorUsuario {
+    /**
+    * Métodos do controlador
+    */
+    
+    /**
+     * @author Rodrigo Zanella Ribeiro
+     * @since 13/10/2013
+     * 
+     * Método para visualizar turmas de um determinado professor
+     */
+    public void visualizarTurmas(){
+        
+    }
+}
