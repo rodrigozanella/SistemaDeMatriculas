ae79257e22f8ff3a6e16a7494361521f112dd3ef
Rodrigo Zanella Ribeiro <rodrigo.zanella@hotmail.com.br>
2013-11-25 22:18:54 -0200

    Adicionando Comandos de transição de estados

---
+++ src/java/Controller/Command/IniciarPeriodoEncomendaComando.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.Command;
+
+import Model.Persistence.DAOs.SistemaDAO;
+import Model.Persistence.FactoryDAO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Zanella
+ */
+public class IniciarPeriodoEncomendaComando implements Comando{
+
+    @Override
+    public void executar(HttpServletRequest request, HttpServletResponse response) {
+        FactoryDAO factory = new FactoryDAO();
+        SistemaDAO sistemaDAO = factory.criarSistemaDAO();
+        String estadoAtual = sistemaDAO.getEstado();
+        if(estadoAtual.equalsIgnoreCase("ferias")){
+            
+        }
+    }
+    
+}
